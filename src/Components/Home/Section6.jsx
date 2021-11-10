356d72be7f0a1622ab07c4d00bf09bfec0697b0e
evanjat2 <ann.jati2@gmail.com>
2021-11-11 06:02:08 +0700

    update logo katalog supporter

--- src/Components/Home/Section6.jsx
+++ src/Components/Home/Section6.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import { useState, useEffect, React } from "react";
+import { db } from "../../data/db";
 import Jadwal from "./Jadwal";
 
 const Section6 = () => {
+
+
+  const [Acara, setAcara] = useState([]);
+  useEffect(() => {
+    const getAcaraFromFirebase = [];
+    const subscriber = db
+      .collection("dataJadwal")
+      .orderBy("time", "asc")
+      .onSnapshot((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          if (doc.data().time.toDate() > new Date()) {
+            getAcaraFromFirebase.push({
+              ...doc.data(), 
+              key: doc.id, 
+            });
+          }
+        });
+        setAcara(getAcaraFromFirebase);
+        console.log(Acara);
+      });
+
+    // return cleanup function
+    return () => subscriber();
+  }, []); // empty dependencies array => useEffect only called once
+
   return (
     <div className="pt-10 relative bg-krem">
-        <h2 className="hidden lg:block text-8xl font-nuku text-merah my-16"> Pertandingan Hari Ini </h2>
-        <div className="pb-20">
-          <Jadwal />
+        <div className={Acara != 0 ? "block" : "hidden"}>
+          <h2 className="hidden lg:block text-8xl font-nuku text-merah my-16"> Pertandingan Terdekat </h2>
+          <div className="pb-20">
+            <Jadwal />
+          </div>
         </div>
     </div>
   );
